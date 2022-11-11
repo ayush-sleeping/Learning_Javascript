41907c8bb3d71afd0abdc6e8ccb077a5ec2140e4
Ayush Mishra <ayushbm84@gmail.com>
2022-11-11 23:46:22 +0530

    Completed some Basics

---
+++ 1. JavaScript Basics/1-3Variables.js
@@ -0,0 +1,31 @@
+console.clear();
+
+var firstnum = 10;
+console.log(firstnum); 
+
+var secondnum = 15;
+console.log(secondnum);
+
+// comment
+// Three 3 different ways of writing variables names.
+
+// camel case 
+var firstNum;
+
+// Pascal case
+var FirstNum;
+ 
+// snake case
+var first_num;
+
+// Rules for naming JS variables
+// - Nmae can contain letters, digits, and $
+var mySubject1, math$
+// - Name cannot start with a digit
+var math;
+// - Name can start with an _ or $
+var _subjectmarks, $rankInClass;
+// - Name are case sentitive
+var x, X;
+// - Name cannot be reserved Keywords
+var varName;
