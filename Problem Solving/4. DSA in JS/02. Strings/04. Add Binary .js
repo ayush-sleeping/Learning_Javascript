c6dab3169ffd893daad02e1d945e1dead3d0535f
Ayush Mishra <ayushbm84@gmail.com>
2025-08-17 20:37:18 +0530

    day 16 of leetcode

---
+++ Problem Solving/4. DSA in JS/02. Strings/04. Add Binary .js
@@ -0,0 +1,47 @@
+/*
+Add Binary
+Given two binary strings a and b, return their sum as a binary string.
+
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+*/
+
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function (a, b) {
+    var i = a.length - 1;
+    var j = b.length - 1;
+    var carry = 0;
+    var result = '';
+
+    while (i >= 0 || j >= 0 || carry) {
+        // Get digit from a or 0 if out of bounds
+        var digitA = i >= 0 ? a[i] - '0' : 0;
+        // Get digit from b or 0 if out of bounds
+        var digitB = j >= 0 ? b[j] - '0' : 0;
+
+        var sum = digitA + digitB + carry;
+        // Current digit is sum % 2
+        result = (sum % 2) + result;
+        // Carry for next step
+        carry = sum >= 2 ? 1 : 0;
+
+        i = i - 1;
+        j = j - 1;
+    }
+    return result;
+};
+
+console.log(addBinary("11", "1"));      // "100"
+console.log(addBinary("1010", "1011")); // "10101"
+console.log(addBinary("0", "0"));       // "0"
+console.log(addBinary("1", "111"));     // "1000"
